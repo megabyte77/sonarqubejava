bfb94770ab74fd7e4bde14b09b8b3e88e6b86f71
Shantanu <jackreacherrt@gmail.com>
2025-02-25 15:11:11 +0530

    java file added

---
+++ src/main/java/com/sonarqubejava/sonarqubejava/SonarqubejavaApplication.java
@@ -0,0 +1,13 @@
+package com.sonarqubejava.sonarqubejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubejavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonarqubejava/sonarqubejava/SonarqubejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonarqubejava.sonarqubejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
